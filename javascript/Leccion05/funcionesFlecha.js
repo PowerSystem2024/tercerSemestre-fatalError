a77d6a723fbc1849519f79429dab77fad97f365e
Morena Ruiz <morenaruiz888@gmail.com>
2025-05-12 15:07:06 -0300

    funcion flecha

---
+++ javascript/Leccion05/funcionesFlecha.js
@@ -0,0 +1,19 @@
+
+miFuncion();
+
+function miFuncion(){
+    console.log("Saluditos desde mi funcion") 
+} 
+
+miFuncion();
+
+let myFuncion = function (){
+    console.log("Saluditos desde la funcion anonima")
+}
+
+//creamos funcion flecha
+let miFuncionFlecha = () => {
+    console.log("Saluditos desde mi funcion flecha");
+}
+//Hay mas variantes para las funciones flechas
+miFuncionFlecha();
