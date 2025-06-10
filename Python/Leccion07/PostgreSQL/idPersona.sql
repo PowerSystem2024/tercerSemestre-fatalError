e89c37c0dd2a41875c163a95501c64c710a15d96
unknown <Andres>
2025-06-10 20:09:28 -0300

    Leccion07

---
+++ Python/Leccion07/PostgreSQL/idPersona.sql
@@ -0,0 +1,6 @@
+--SELECT * FROM persona WHERE id_persona = 1
+--INSERT INTO persona (nombre, apellido, email)VALUES ('Andy', 'Dmz', 'admz@mail.com')
+--SELECT * FROM persona
+--UPDATE persona SET nombre = 'Ivonne',email = 'iesparaza@mail.com', apellido = 'Esparza' WHERE id_persona = 1
+SELECT * FROM persona
+--DELETE FROM persona WHERE id_persona = 1
